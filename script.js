8ef26285491117ace09f64e3256631ba9e3302b5
Kurmanbekov2006 <kumanbekoverbol0000@gmail.com>
2026-04-08 01:14:36 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function updateTime() {
+  const now = new Date();
+  document.getElementById("time").textContent =
+    now.toLocaleTimeString();
+}
+
+setInterval(updateTime, 1000);
+updateTime();
